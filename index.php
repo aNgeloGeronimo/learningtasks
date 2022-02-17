c70d784f06d9b146825c463c08d7b056762c2c44
aNgeloGeronimo <99866468+aNgeloGeronimo@users.noreply.github.com>
2022-02-17 15:58:39 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Home Page</title>
+  </head>
+  
+  <body>
+    <h1> Welcome to my Website!! I am your mom </h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="/LT2/index.html">Learning Task 2</a></li>
+    </ul>
+  </body>
+</html>
