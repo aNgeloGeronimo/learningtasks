1800da5f25853fd9566d2f558c0b6626f35eb282
aNgeloGeronimo <99866468+aNgeloGeronimo@users.noreply.github.com>
2022-02-17 17:40:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
       <li><a href="index.php">HOME</a></li>
       <li><a href="/LT2/index.html">Learning Task 2</a></li>
       <li><a href="recipe1.html">Chicken Curry</a></li>
+      <li><a href="recipe2.html">Lasagna</a></li>
+      <li><a href="recipe3.html">Chicken Adobo</a></li>
+      <li><a href="recipe4.html">Pork Humba</a></li>
+      <li><a href="recipe5.html">Sinigang</a></li>
+      <li><a href="recipe6.html">Bakareta</a></li>
+      <li><a href="recipe7.html">Dinuguan</a></li>
     </ul>
   </body>
 </html>
